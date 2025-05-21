1d7b62a846aa0ec2f19bc68379142570a57ed4cd
Đặng Công Diện <141894358+congdien19@users.noreply.github.com>
2025-05-21 16:02:52 +0700

    Update Menu.js

--- src/components/Header/Menu.js
+++ src/components/Header/Menu.js
@@ -2,25 +2,18 @@ import React, { useState } from 'react';
 import './Menu.css';
 
 function Menu({ onMenuClick }) {
-  const [activeItem, setActiveItem] = useState('Home');
-  const [hoveredItem, setHoveredItem] = useState(null);
-  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+  const [activeItem, setActiveItem] = useState('Home'); // Mặc định là Home
+  const [hoveredItem, setHoveredItem] = useState(null); // Trạng thái cho mục đang hover
 
   const handleClick = (page) => {
     setActiveItem(page);
     onMenuClick(page);
-    setIsMobileMenuOpen(false); // Ẩn menu sau khi chọn trên mobile
-  };
-
-  const toggleMobileMenu = () => {
-    setIsMobileMenuOpen(!isMobileMenuOpen);
   };
 
   return (
     <nav className="menu">
-      <div className="menu-toggle" onClick={toggleMobileMenu}></div>
-      <ul className={isMobileMenuOpen ? 'open' : ''}>
-        {['Home', 'Hat', 'Shirt', 'Introduce', 'Contact'].map((item) => (
+      <ul>
+        {['Home', 'Hat', 'Shirt','Introduce','Contact'].map((item) => (
           <li
             key={item}
             className={`${activeItem === item ? 'active' : ''} ${hoveredItem === item ? 'hovered' : ''}`}
